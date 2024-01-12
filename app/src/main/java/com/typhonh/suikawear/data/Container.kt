adf51bd4b483a001b33d59b02e37eb753182c2fa
Harrison Tyson <hty13@uclive.ac.nz>
2024-01-12 19:37:41 +1300

    Shift positioning to better fit in frame

--- app/src/main/java/com/typhonh/suikawear/data/Container.kt
+++ app/src/main/java/com/typhonh/suikawear/data/Container.kt
@@ -3,6 +3,6 @@ package com.typhonh.suikawear.data
 data class Container(
     val height: Float = 0.72f,
     val width: Float = 0.6f,
-    override var posY: Float = 0.15f,
+    override var posY: Float = 0.05f,
     override var posX: Float = 0f
 ) : UiObject
--- app/src/main/java/com/typhonh/suikawear/data/Fruit.kt
+++ app/src/main/java/com/typhonh/suikawear/data/Fruit.kt
@@ -2,6 +2,6 @@ package com.typhonh.suikawear.data
 
 data class Fruit(
     override var posX: Float = 0f,
-    override val posY: Float = -0.75f,
+    override val posY: Float = -0.8f,
     val radius: Float = 0.045f
 ) : UiObject
