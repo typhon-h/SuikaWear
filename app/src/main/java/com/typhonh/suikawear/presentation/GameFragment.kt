dc39195b306dc20ff5233c235953d93b12a7cb39
Harrison Tyson <hty13@uclive.ac.nz>
2024-01-28 12:48:56 +1300

    Add game over screen

--- app/src/main/java/com/typhonh/suikawear/presentation/GameFragment.kt
+++ app/src/main/java/com/typhonh/suikawear/presentation/GameFragment.kt
@@ -14,15 +14,19 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -42,6 +46,8 @@ fun GameFragment(
     dataStore: DataStore<Preferences>,
     viewModel: GameEngineViewModel = viewModel { GameEngineViewModel(GameState(), dataStore) }
 ) {
+    val state by viewModel.uiState.collectAsState()
+
     GameCanvas(viewModel = viewModel)
 
     var showClearConfirmation by remember { mutableStateOf(false) }
@@ -153,6 +159,46 @@ fun GameFragment(
             BackHandler(
                 enabled = true,
                 onBack = { showBackConfirmation = false }
+            )
+        }
+    }
+
+    if(state.hasEnded) {
+        Alert(
+            backgroundColor = Color.Black.copy(alpha = 0.5f),
+            title = {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                        Text("Game Over", style = MaterialTheme.typography.headlineLarge)
+                        Text(state.score.toString(), fontSize = 30.sp, style = androidx.wear.compose.material.MaterialTheme.typography.title1.copy(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(Color(250, 250, 220), Color(240, 200, 30))
+                            ),))
+                    }
+            },
+            negativeButton = {
+                Button(
+                    onClick = { navigate(Screen.HomeScreen.route, NavOptions.Builder().setPopUpTo(Screen.HomeScreen.route, true).build()) }) {
+                    Icon(
+                        Icons.Default.ArrowBack,
+                        "Back to Menu"
+                    )
+                }
+            },
+            positiveButton = {
+                Button(
+                    onClick = {
+                        viewModel.resetGame()
+                    }) {
+                    Icon(
+                        Icons.Default.Refresh,
+                        "Play Again"
+                    )
+                }
+            },
+        ) {
+            BackHandler(
+                enabled = true,
+                onBack = { viewModel.resetGame() }
             )
         }
     }
